5ee0f67f2db010ca452d631c92c03a118adb8ed5
Sourav Singh <commitsovercoffee@gmail.com>
2023-05-07 21:55:46 +0530

    replaces liberation with firacode-nerd font

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:size=18:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:size=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
